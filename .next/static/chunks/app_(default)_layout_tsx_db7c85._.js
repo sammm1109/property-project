2f787399d8a9b82d180d6d145ddb479224b71cff
sammm1109 <ernjie1109@gmail.com>
2025-05-17 17:56:06 +0800

    update changes

---
+++ .next/static/chunks/app_(default)_layout_tsx_db7c85._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(default)_layout_tsx_db7c85._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(default)_layout_tsx_db7c85._.js",
+  "chunks": [
+    "static/chunks/node_modules_442c80._.js",
+    "static/chunks/_c32d02._.js",
+    "static/chunks/node_modules_aos_dist_aos_941406.css"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_(default)_page_tsx_246670._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(default)_page_tsx_246670._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(default)_page_tsx_246670._.js",
+  "chunks": [
+    "static/chunks/node_modules_swiper_c900ba._.js",
+    "static/chunks/_a6233d._.js"
+  ],
+  "source": "dynamic"
+});
