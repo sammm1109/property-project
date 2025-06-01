35e3af9ea436a3c005164d23849aea761f2eb3bb
sammm1109 <ernjie1109@gmail.com>
2025-06-01 14:44:44 +0800

    test separate project

---
+++ pavilion-square/components/home.tsx
@@ -0,0 +1,31 @@
+"use client";
+import HeaderImg from "@/public/images/property/header.jpg";
+import Logo from "@/public/images/property/logo_big.png";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <section
+      id="home"
+      className="relative bg-no-repeat bg-center bg-cover h-[70vh]"
+      style={{
+        backgroundImage: `url(${HeaderImg.src})`,
+      }}
+    >
+      <div className="mx-auto max-w-6xl px-4 sm:px-6 ">
+        <div className="pb-12 pt-[40%] md:pb-20 md:pt-[15%]">
+          {/* Logo */}
+          <div className="relative flex justify-center">
+            <Image
+              className="object-cover"
+              src={Logo}
+              width={350}
+              height={350}
+              alt="Logo_big"
+            />
+          </div>
+        </div>
+        <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-[#ffe0cb] to-transparent z-0 pointer-events-none"></div>
+      </div>
+    </section>
+  );
+}
---
+++ pavilion-square/components/luxury-residence.tsx
@@ -0,0 +1,59 @@
+"use client";
+import LuxuryImg from "@/public/images/property/luxury.jpg";
+import WhatsappImg from "@/public/images/property/whatsapp.png";
+import { openWhatsAppMessage } from "./common/manager";
+import { Button } from "./common/Button";
+import Image from "next/image";
+
+export default function LuxuryResidence() {
+  return (
+    <section id="luxury-residences">
+      <div className="bg-[#371807] ">
+        <div className="mx-auto max-w-6xl px-4 py-10 sm:px-6 text-center">
+          <div className="grid grid-cols-12 gap-5">
+            <div className="relative col-span-12 md:col-span-6 w-full h-[500px] md:h-[600px]">
+              <Image
+                className="object-cover shadow-2xl "
+                style={{ boxShadow: "0 4px 20px #fff" }}
+                src={LuxuryImg}
+                fill
+                alt="corporate_img"
+                data-aos="zoom-in"
+                data-aos-delay={120}
+              />
+            </div>
+            <div
+              className="col-span-12 md:col-span-6 flex items-center justify-center"
+              data-aos="fade-left"
+              data-aos-delay={300}
+            >
+              <div className="grid text-white gap-5">
+                <span
+                  style={{ fontFamily: "ACaslonPro-Regular" }}
+                  className="text-orange-500 text-5xl font-medium"
+                >
+                  LUXURY RESIDENCE
+                </span>
+                <p className="text-xl font-medium">
+                  Pavilion Square’s impressive residential tower evokes a sense
+                  of wonder and prominence. The residence features 960 units,
+                  from 504 sq.ft. studios to 1,272 sq.ft. suites, catering to
+                  various needs.
+                </p>
+
+                <div className="flex justify-center">
+                  <Button
+                    label="LATEST PRICING"
+                    onClickFunction={openWhatsAppMessage}
+                    alt="whatsapp"
+                    src={WhatsappImg}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
